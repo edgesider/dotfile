1e7c3befb2afc8849e7aa41b067991e05f320045
ykai <yingkaidang@gmail.com>
2019-04-04 14:58:21 +0800

    update

--- out/fish/config.fish
+++ out/fish/config.fish
@@ -1,7 +1,8 @@
-alias p "proxychains -q"
+alias pc "proxychains"
+alias po "proxyon"
+alias pf "proxyoff"
 set fish_greeting
 set -gx PATH ~/.local/bin $PATH
 set -x EDITOR vim
 set -x GOPATH $HOME/go
 alias s "sh -c 'sslocal -c $HOME/ss/jd_ss.json 1> /tmp/ss.log 2>&1 & proxychains -f $HOME/ss/2080.conf python $HOME/ss/shadowsocksr/shadowsocks/local.py -c $HOME/.config/ssr/USING.JSON 1> /tmp/ssr.log 2>&1'"
-
